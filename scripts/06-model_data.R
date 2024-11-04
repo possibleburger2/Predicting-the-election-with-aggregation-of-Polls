b49dc06e74c25af3a4bfbea4f395348a45b6b16c
possibleburger2 <globalchucknorris@gmail.com>
2024-11-04 17:09:27 -0500

    Update 06-model_data.R

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,11 +1,9 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models winner of 2024 US election
+# Author: Daniel, Vandan
+# Date: 4 November 2024
+# Contact: danie.xu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####

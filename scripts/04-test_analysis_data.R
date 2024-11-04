add6477f43fe0e989f14cca3c7d1c3deda0decc8
possibleburger2 <globalchucknorris@gmail.com>
2024-11-04 17:07:09 -0500

    Update 04-test_analysis_data.R

--- scripts/04-test_analysis_data.R
+++ scripts/04-test_analysis_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 26 September 2024 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests analysis data
+# Author: Daniel, Vandan
+# Date: 4 November 2024
+# Contact: danie.xu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 
 #### Workspace setup ####
